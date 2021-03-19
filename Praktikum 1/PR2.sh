30bd150ee0d8c50d690880c8a5153711d013773b
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-03-19 14:55:24 +0700

    Update PR2.sh

--- Praktikum 1/PR2.sh
+++ Praktikum 1/PR2.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-du -sh /home/bayu/Downloads > "/home/bayu/log/$(date +"%a %b %d %T %Z %Y").txt"
 
-
-
-
-
+*/15 8-17 * * * du -sh /home/bayu/Downloads > "/home/bayu/log/$(date +"\%a \%b \%d \%T \%Z \%Y").txt
