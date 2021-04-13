3ffe199441aa4c4449a1cf36c4c2444bb7a1443d
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-04-13 15:44:29 +0700

    Update no2.c

--- Praktikum2/no2.c
+++ Praktikum2/no2.c
@@ -1,34 +1,36 @@
-#include <stdlib.h>
-#include <sys/types.h>
-#include <time.h>
-#include <unistd.h>
-#include <wait.h>
-
-int main()
-{
-    pid_t child;
-    int status;
-    time_t now;
-    time(&now);
-    now = localtime(&now);
-    char[100] StrTime;
-    strftime(StrTime, 100, "%d-%m-%Y_%X", now);
-
-    child = fork() if (child < 0)
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    if (child == 0)
-    {
-        char *argv[] = {"mkdir", "-p",StrTime, NULL};
-        execv("/bin/mkdir", argv);
-    }
-    else
-    {
-        // this is parent
-        while ((wait(&status)) > 0);
-        char *argv[] = {"cp","-R", "/home/bayu/Downloads/.", NULL};
-        execv("/usr/bin/cp", argv);
-    }
-}
+ #include <stdlib.h>
+ #include <sys/types.h>
+ #include <time.h>
+ #include <unistd.h>
+ #include <wait.h>
+ 
+ int main()
+ {
+     pid_t child;
+     int status;
+     time_t now;
+     time(&now);
+     struct tm *timern;
+     timern = localtime(&now);
+     char StrTime[100];
+     strftime(StrTime, 100, "%d-%m-%Y_%X", timern);
+ 
+     child = fork();
+     if (child < 0)
+     {
+         exit(EXIT_FAILURE);
+     }
+ 
+     if (child == 0)
+     {
+         char *argv[] = {"mkdir", "-p",StrTime, NULL};
+         execv("/bin/mkdir", argv);
+     }
+     else
+     {
+         // this is parent
+         while ((wait(&status)) > 0);
+         char *argv[] = {"cp","-R", "/home/bayu/Downloads/.",StrTime, NULL};
+         execv("/usr/bin/cp", argv);
+     }
+ }
